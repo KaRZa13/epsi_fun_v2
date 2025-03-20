618cb2b1f1da5ccfe8aa2701eb80092b47a47431
Rafael MURO <rafael.francisco.muro@gmail.com>
2025-03-20 17:05:57 +0100

    2025/03/20 - Home page almost done

---
+++ components/Topbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="w-full inline-flex justify-center items-center font-sniglet-400 py-4">
+    <div class="flex flex-col items-center justify-center">
+      <span class="text-6xl">
+        EPSI.FUN
+      </span>
+      <span class="text-2xl">
+        Lorem ipsum dolor sit amet.
+      </span>
+    </div>
+    <div class="absolute inline-flex space-x-2 items-center right-8 top-4">
+      <img src="/img/utils/mail.png" class="h-5">
+      <label>Get new posts</label>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    COUCOU
-  </div>
+  <Topbar />
+
+  <section class="flex justify-center items-center">
+
+    <div class="flex flex-wrap justify-center items-center gap-4 pt-16 max-w-[1400px] w-full font-sniglet-400">
+
+      <NuxtLink v-for="game in games" :key="game.id" :to="`/${game.url}`"
+        class="relative flex flex-wrap w-[300px] h-[200px] border rounded-2xl overflow-hidden">
+        <img :src="game.img" :alt="game.name" class="w-full h-full object-cover hover:scale-110 transition-opacity duration-500" />
+        <div class="absolute bottom-0 p-2 bg-black/50 text-white">
+          {{ game.name }}
+        </div>
+      </NuxtLink>
+
+    </div>
+  </section>
 </template>
+
+<script setup lang="ts">
+import games from '../assets/json/games.json'
+</script>
