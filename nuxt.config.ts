8fc2cf4e3ad3e60f7a33f774f03df46118d07945
Rafael MURO <rafael.francisco.muro@gmail.com>
2025-04-18 18:45:10 +0200

    2025/04/18 - Module nuxt fixed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,10 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  modules: ['@nuxt/fonts', '@nuxt/icon, @nuxtjs/supabase'],
+  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxtjs/supabase'],
+  supabase: {
+    redirect: false,
+  },
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
